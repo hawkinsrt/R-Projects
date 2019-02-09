c221d26a6dcd8d00bc484b6908839d648b646d26
Ryan Hawkins <hawkinsrt@vcu.edu>
2019-02-09 13:08:28 -0500

    Now works with 2 years of data.  I guess we can always just run the code 3 times then compile it all.

--- Masters Practicum/members_Cleaner_Code_Work_In_Progress.R
+++ Masters Practicum/members_Cleaner_Code_Work_In_Progress.R
@@ -1,12 +1,7 @@
 library(tidyverse)
+library(naniar)
 
-# Import and edit claims data -  Ryan's code
-claimsFull <- read_rds("Data/claimsCleanSmall.RDS")
-
-claimsFull<- claimsFull %>% 
-  filter(PLACE_OF_SERVICE_DESC != 'EMERGENCY ROOM - HOSPITAL'| is.na(PLACE_OF_SERVICE_DESC))
-
-all_diag_df <- claimsFull %>% 
+all_diag_df <- claims %>% 
   select(starts_with('CODE_'))
 
 all_diag <- c(t(all_diag_df))
@@ -28,41 +23,43 @@ icd9vector <- icd9Only$all_diag
 
 # import diagnosis_final
 diag <- read_csv('Data/diagnosis_final.csv')
-
+miss_var_summary(diag)
+diag$BODY_SYSTEM[is.na(diag$BODY_SYSTEM)] <- 0
+unique(diag$BODY_SYSTEM)
 #==============================================
-# unite claim num and episode_seq to get a unique ID. needed for joins and spreads
-# select the bare minimum columns for memeory sake, for the same reason take half the years
-claimsFull <- claimsFull %>%
+
+# Import and edit claims data -  Ryan's code
+claims <- read_rds("Data/claimsCleanSmall.RDS")
+
+claims<- claims %>% 
+  filter(PLACE_OF_SERVICE_DESC != 'EMERGENCY ROOM - HOSPITAL'| is.na(PLACE_OF_SERVICE_DESC)) %>% 
   unite(CLAIM_NUM, EPISODE_SEQ, col = ID, sep = '-') %>% 
-  select(ID, starts_with('CODE_'),YEAR) %>%
+  select(ID, starts_with('CODE_'),YEAR) %>% 
   filter(YEAR %in% c('2013','2014'))
 
 # gather all codes into one column so you only have to mutate once
-claimsFull2 <- claimsFull %>% 
+claims2 <- claims %>% 
   gather(number, DIAG_CODE, starts_with('CODE_')) %>% 
   mutate(ICD_VERSION = ifelse(DIAG_CODE %in% icd9vector, 9,10),
           DIAG_CODE = str_remove_all(DIAG_CODE, pattern = "[[:punct:]]"))
 
+rm(claims)
+
 # group by diag code and icd version then join the diagnosis_final dataset to get cci and body system
-claimsFull3 <- claimsFull2 %>% 
-  group_by(DIAG_CODE, ICD_VERSION) %>% 
-  left_join(diag, by = c('DIAG_CODE' = 'DIAG_CODE', 'ICD_VERSION' = 'ICD_VERSION')) %>% 
-  ungroup()
+claims3 <- claims2 %>% 
+  #group_by(DIAG_CODE, ICD_VERSION) %>% 
+  left_join(diag, by = c('DIAG_CODE' = 'DIAG_CODE', 'ICD_VERSION' = 'ICD_VERSION'))
 
 # free up some memory
-rm(claimsFull)
-rm(claimsFull2)
+rm(claims2)
+gc()
 
-# this is when the spread comes in
-# for some reason this throws an error about duplicate rows but ID should be a unique identifier
-# I wonder if its because I had to remove the NAs in the gather statement to save memeory and 
-# when spreading back out now that those columns don't exsist it gets mad.  
-# I used Unite to get a combo body system chronic yes/no in one column just to try and spread SOMETHING
-claimsFull4 <- claimsFull3 %>% 
-  select(ID, number, CHRONIC_INDICATOR, BODY_SYSTEM) %>% 
-  unite(BODY_SYSTEM, CHRONIC_INDICATOR, col = 'CHRONIC_BDSYS', sep = '-') %>% 
-  spread(number, CHRONIC_BDSYS)
+claims4 <- claims3 %>% 
+  select(ID, number, BODY_SYSTEM) %>% 
+  spread(number, BODY_SYSTEM)
 
-
-
+miss_var_summary(claims4)
+rm(claims3)
+rm(claims4)
+gc()
 
